84cc8f4ea30d4da20d3bad30cbcd0116fcb0da4a
glerison <glerison@192.168.15.13>
2017-07-31 21:47:19 -0300

    aula de remoção de entidades

--- src/main/java/br/com/financeiro/controller/EntidadesController.java
+++ src/main/java/br/com/financeiro/controller/EntidadesController.java
@@ -4,11 +4,15 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.financeiro.model.Entidade;
+import br.com.financeiro.repository.EntidadesRepository;
 import br.com.financeiro.services.EntidadeService;
 
 @Controller
@@ -20,20 +24,36 @@ public class EntidadesController {
 	@Autowired
 	private EntidadeService entidadeService;
 	
+	@Autowired
+	private EntidadesRepository repository;
+	
 	@RequestMapping("/novo")
-	public String novo() {
+	public String novo(Entidade entidade) {
 		return INDEX;
 	}
 	
 	@RequestMapping(value="/novo", method=RequestMethod.POST)
 	public String salvar(@Valid Entidade entidade, BindingResult result) {
 		if(result.hasErrors()) {
-			System.out.println("Tem erro! "+result.getAllErrors().toString());
-			return novo();
+			return novo(entidade);
 		}
 		
 		System.out.println("Entidade: " +entidade.toString());
 		entidadeService.salvar(entidade);
-		return novo();
+		return "redirect:/entidades/novo";
+	}
+	
+	@RequestMapping
+	public String pesquisar(Entidade entidade, Model model) {
+		String nome = entidade.getNome() == null ? "%" : entidade.getNome();
+		model.addAttribute("entidades", repository.findByNomeContainingIgnoreCase(nome));
+		return "entidade/PesquisarEntidade";
+	}
+	
+	@RequestMapping("/{codigo}")
+	public ModelAndView editar(@PathVariable("codigo") Entidade entidade) {
+		ModelAndView mv =  new ModelAndView(INDEX);
+		mv.addObject(entidade);
+		return mv;
 	}
 }
--- src/main/java/br/com/financeiro/repository/EntidadesRepository.java
+++ src/main/java/br/com/financeiro/repository/EntidadesRepository.java
@@ -1,5 +1,9 @@
 package br.com.financeiro.repository;
 
+
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +12,5 @@ import br.com.financeiro.model.Entidade;
 @Repository
 public interface EntidadesRepository extends JpaRepository<Entidade, Long>{
 
+	List<Entidade> findByNomeContainingIgnoreCase(String nome);
 }
